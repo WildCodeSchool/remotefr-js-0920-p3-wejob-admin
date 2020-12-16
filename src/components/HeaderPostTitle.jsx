b4dbda493bf78dfcfffcfc80d632714fb9eef370
Emilie <martelemilie@hotmail.fr>
2020-12-16 11:35:36 +0100

    initialize the project, add proptypes, create the HeaderPostTitle component with css

---
+++ src/components/HeaderPostTitle.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function HeaderPostTitle() {
+  return (
+    <div className="header-post-title-container clearfix">
+      <div className="inner-wrap">
+        <div className="post-title-wrapper">
+          <h1 className="header-post-title-class entry-title">
+            Prestations RH aux entreprises
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderPostTitle;
