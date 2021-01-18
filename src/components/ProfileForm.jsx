b6528ec805822e966b0de7f20927e9f782173599
Emilie <martelemilie@hotmail.fr>
2021-01-18 23:55:21 +0100

    fix bug to send back the data entered by the user

--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -1,69 +1,125 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react'; // { useContext }
+import PropTypes from 'prop-types';
 
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+// import InputFormField from './widgetsFormField/InputFormField';
+// import DataFormContext from '../contexts/DataFormContext';
 
-import InputFormField from './InputFormField';
-
-function ProfileForm() {
-  const schema = yup.object().shape({
-    lastname: yup.string().min(2).required('Vous devez entrer votre nom'),
-    firstname: yup.string().required(),
-    email: yup.string().email().required(),
-  });
-
-  const { register, handleSubmit } = useForm({
-    mode: 'onTouched',
-    resolver: yupResolver(schema),
-  });
-
-  const onSubmit = (data) => {
-    return JSON.stringify(data);
-  };
-
+function ProfileForm({ register, handleSubmit, errors }) {
   return (
-    <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
+    <form className="ProfileForm container py-5" onSubmit={handleSubmit}>
       <h3 className="widget-title">Mon profil</h3>
       <hr />
       <fieldset id="coordonnees">
         <legend htmlFor="Civility" className="form-field-label">
           Civilité *
         </legend>
-        <InputFormField
-          label="M"
-          name="Civility"
-          type="radio"
-          register={register}
-        />
-        <InputFormField
-          label="Mme"
-          name="Civility"
-          type="radio"
-          register={register}
-        />
+        <div className="row">
+          <div className="form-check form-check-inline">
+            <label htmlFor="male" className="form-field-label col-md-12">
+              Monsieur
+              <input
+                type="radio"
+                className="form-field-input"
+                id="male"
+                name="gender"
+                value="M"
+                ref={register}
+              />
+            </label>
+
+            <label htmlFor="female" className="form-field-label col-md-12">
+              Madame
+              <input
+                type="radio"
+                className="form-field-input"
+                id="female"
+                name="gender"
+                value="Mme"
+                ref={register}
+              />
+            </label>
+          </div>
+        </div>
+        {errors.gender && (
+          <span className="spanError">{errors.gender.message}</span>
+        )}
       </fieldset>
 
-      <InputFormField
-        label="NOM *"
-        name="lastname"
-        type="text"
-        register={register}
-      />
-      <InputFormField
-        label="Prénom *"
-        name="firstname"
-        type="text"
-        register={register}
-      />
-      <InputFormField
-        label="Email"
-        name="email"
-        type="email"
-        register={register}
-      />
+      <div className="row">
+        <div className="form-group">
+          <label htmlFor="firstname" className="form-field-label">
+            Prénom *
+            <input
+              type="text"
+              className="form-field-input"
+              id="firstname"
+              name="firstname"
+              ref={register}
+            />
+          </label>
+          {errors.firstname && (
+            <span className="spanError">{errors.firstname.message}</span>
+          )}
+        </div>
+      </div>
+      <div className="row">
+        <div className="form-group">
+          <label htmlFor="lastname" className="form-field-label">
+            Nom *
+            <input
+              type="text"
+              className="form-field-input"
+              id="lastname"
+              name="lastname"
+              ref={register}
+            />
+          </label>
+          {errors.lastname && (
+            <span className="spanError">{errors.lastname.message}</span>
+          )}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email" className="form-field-label">
+            Email
+            <input
+              type="email"
+              className="form-field-input"
+              id="email"
+              name="email"
+              placeholder="name@example.com"
+              ref={register}
+              readOnly
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Valider et poursuivre
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
+
+ProfileForm.propTypes = {
+  register: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  errors: PropTypes.shape({
+    gender: PropTypes.shape({
+      message: PropTypes.string,
+      type: PropTypes.string,
+    }),
+    firstname: PropTypes.shape({
+      message: PropTypes.string,
+      type: PropTypes.string,
+    }),
+    lastname: PropTypes.shape({
+      message: PropTypes.string,
+      type: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default ProfileForm;
--- src/pages/JobeurForm/index.jsx
+++ src/pages/JobeurForm/index.jsx
@@ -1,28 +1,96 @@
-import React from 'react';
-import MultiStep from 'react-multistep';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+// import useFormPersist from 'react-hook-form-persist';
+import MultiStep from '../../components/MultiStepFormField';
 import HeaderPostTitle from '../../components/HeaderPostTitle';
+
 import ProfileForm from '../../components/ProfileForm';
 import TrainingForm from '../../components/TrainingForm';
 import ExperiencesForm from '../../components/ExperiencesForm';
-
-const steps = [
-  { name: 'Profil', component: <ProfileForm /> },
-  { name: 'Formations', component: <TrainingForm /> },
-  { name: 'Expériences', component: <ExperiencesForm /> },
-  { name: 'Recruteurs', component: 'Informations recruteurs' },
-  {
-    name: 'Documents',
-    component: 'Télechargement de documents',
-  },
-];
+import RecruitersInfoForm from '../../components/RecruitersInfoForm';
 
 function JobeurForm() {
+  const schema = yup.object().shape({
+    gender: yup.string().required('Vous devez sélectionner votre genre'),
+    lastname: yup.string().min(2).required('Vous devez entrer votre nom'),
+    firstname: yup.string().required('Vous devez entrer votre prénom'),
+    email: yup.string().email(),
+    diploma: yup.string(),
+    levelOfExperience: yup.string(),
+    languages: yup.string(),
+    activityArea: yup.string(),
+    jobName: yup.string(),
+    skills: yup.string(),
+    availability: yup.string().required(),
+    modility: yup.string().required(),
+    textDescription: yup.string().required(),
+  });
+
+  const { register, handleSubmit, errors } = useForm({
+    mode: 'onTouched',
+    resolver: yupResolver(schema),
+  });
+
+  // Retrieves user entries
+  const [dataForm, setDataForm] = useState([]);
+
+  // Button function valid and continue
+  const onSubmit = (data) => {
+    // eslint-disable-next-line no-console
+    console.log('data : ', data);
+    setDataForm({ ...dataForm, ...data });
+    // eslint-disable-next-line no-console
+    console.log(data, dataForm);
+  };
+
+  const steps = [
+    {
+      name: 'Profil',
+      component: (
+        <ProfileForm
+          register={register}
+          handleSubmit={handleSubmit(onSubmit)}
+          errors={errors}
+        />
+      ),
+    },
+    {
+      name: 'Formations',
+      component: <TrainingForm register={register} />,
+    },
+    {
+      name: 'Expériences',
+      component: <ExperiencesForm register={register} />,
+    },
+    {
+      name: 'Recruteurs',
+      component: <RecruitersInfoForm register={register} />,
+    },
+    {
+      name: 'Documents',
+      component: 'Télechargement de documents',
+    },
+  ];
+
+  // useFormPersist(
+  //   'formJobeur',
+  //   { watch, setValue },
+  //   {
+  //     storage: window.localStorage,
+  //   },
+  // );
+
+  // eslint-disable-next-line no-console
+  console.log('dataForm : ', dataForm);
+
   return (
     <div className="main-wrapper">
       <HeaderPostTitle name="Formulaire candidat" />
       <div className="single-page clearfix">
         <div className="inner-wrap">
-          <MultiStep showNavigation steps={steps} />
+          <MultiStep steps={steps} />
         </div>
       </div>
     </div>
