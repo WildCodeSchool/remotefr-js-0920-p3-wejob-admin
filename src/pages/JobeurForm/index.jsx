4327a2b56e6c1ae52d98942abfb8ba4e87820e21
Emilie <martelemilie@hotmail.fr>
2021-01-19 14:57:21 +0100

    replace yup schema with useState, add useState to determine the current form step, update onSubmit function, and props in steps.component and Multistep

--- src/pages/JobeurForm/index.jsx
+++ src/pages/JobeurForm/index.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-// import useFormPersist from 'react-hook-form-persist';
 import MultiStep from '../../components/MultiStepFormField';
 import HeaderPostTitle from '../../components/HeaderPostTitle';
 
@@ -12,61 +11,92 @@ import ExperiencesForm from '../../components/ExperiencesForm';
 import RecruitersInfoForm from '../../components/RecruitersInfoForm';
 
 function JobeurForm() {
-  const schema = yup.object().shape({
-    gender: yup.string().required('Vous devez sélectionner votre genre'),
-    lastname: yup.string().min(2).required('Vous devez entrer votre nom'),
-    firstname: yup.string().required('Vous devez entrer votre prénom'),
-    email: yup.string().email(),
-    diploma: yup.string(),
-    levelOfExperience: yup.string(),
-    languages: yup.string(),
-    activityArea: yup.string(),
-    jobName: yup.string(),
-    skills: yup.string(),
-    availability: yup.string().required(),
-    modility: yup.string().required(),
-    textDescription: yup.string().required(),
-  });
+  // update the validation  yup schema for the data entered by the user when changing the form step
+  const [schema, setSchema] = useState(yup.object().shape({}));
+
+  // const schema = yup.object().shape({
+  //   gender: yup.string().required('Vous devez sélectionner votre genre'),
+  //   lastname: yup.string().min(2).required('Vous devez entrer votre nom'),
+  //   firstname: yup.string().required('Vous devez entrer votre prénom'),
+  //   email: yup.string().email(),
+  //   diploma: yup.string(),
+  //   levelOfExperience: yup.string(),
+  //   languages: yup.string(),
+  //   activityArea: yup.string(),
+  //   jobName: yup.string(),
+  //   skills: yup.string(),
+  //   availability: yup.string().required(),
+  //   modility: yup.string().required(),
+  //   textDescription: yup.string().required(),
+  // });
 
   const { register, handleSubmit, errors } = useForm({
     mode: 'onTouched',
     resolver: yupResolver(schema),
   });
 
+  const [compState, setComp] = useState(0);
+
   // Retrieves user entries
-  const [dataForm, setDataForm] = useState([]);
+  const [dataForm, setDataForm] = useState({});
 
   // Button function valid and continue
   const onSubmit = (data) => {
     // eslint-disable-next-line no-console
     console.log('data : ', data);
+    // 1. Records the data entered by the user
     setDataForm({ ...dataForm, ...data });
-    // eslint-disable-next-line no-console
-    console.log(data, dataForm);
+    // 2. Sends data to the database
+
+    // 3. Go to the next step in the form
+    setComp(compState + 1);
   };
 
   const steps = [
     {
       name: 'Profil',
+      nameForm: 'ProfileForm',
       component: (
         <ProfileForm
           register={register}
           handleSubmit={handleSubmit(onSubmit)}
           errors={errors}
+          setSchema={setSchema}
         />
       ),
     },
     {
       name: 'Formations',
-      component: <TrainingForm register={register} />,
+      component: (
+        <TrainingForm
+          register={register}
+          handleSubmit={handleSubmit(onSubmit)}
+          errors={errors}
+          setSchema={setSchema}
+        />
+      ),
     },
     {
       name: 'Expériences',
-      component: <ExperiencesForm register={register} />,
+      component: (
+        <ExperiencesForm
+          register={register}
+          handleSubmit={handleSubmit(onSubmit)}
+          errors={errors}
+          setSchema={setSchema}
+        />
+      ),
     },
     {
       name: 'Recruteurs',
-      component: <RecruitersInfoForm register={register} />,
+      component: (
+        <RecruitersInfoForm
+          register={register}
+          handleSubmit={handleSubmit(onSubmit)}
+          errors={errors}
+          setSchema={setSchema}
+        />
+      ),
     },
     {
       name: 'Documents',
@@ -74,23 +104,12 @@ function JobeurForm() {
     },
   ];
 
-  // useFormPersist(
-  //   'formJobeur',
-  //   { watch, setValue },
-  //   {
-  //     storage: window.localStorage,
-  //   },
-  // );
-
-  // eslint-disable-next-line no-console
-  console.log('dataForm : ', dataForm);
-
   return (
     <div className="main-wrapper">
       <HeaderPostTitle name="Formulaire candidat" />
       <div className="single-page clearfix">
         <div className="inner-wrap">
-          <MultiStep steps={steps} />
+          <MultiStep steps={steps} compState={compState} setComp={setComp} />
         </div>
       </div>
     </div>
