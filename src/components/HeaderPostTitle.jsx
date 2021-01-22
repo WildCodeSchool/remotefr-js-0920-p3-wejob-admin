0094cad49d37d72c0aeaa7b942e57d40894c4f27
Emilie <martelemilie@hotmail.fr>
2021-01-22 16:27:09 +0100

    Add style in HeaderPostTitle

--- src/components/HeaderPostTitle.jsx
+++ src/components/HeaderPostTitle.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from '../asset/css/HeaderPostTitle.module.css';
 
 function HeaderPostTitle(props) {
   const { name } = props;
   return (
-    <div className="header-post-title-container clearfix">
-      <div className="inner-wrap">
-        <div className="post-title-wrapper">
-          <h1 className="header-post-title-class entry-title">{name}</h1>
+    <div className={styles.headerPostTitleContainer}>
+      <div className={styles.innerWrap}>
+        <div className={styles.postTitleWrapper}>
+          <h1 className={styles.headerPostTitleClass}>{name}</h1>
         </div>
       </div>
     </div>
