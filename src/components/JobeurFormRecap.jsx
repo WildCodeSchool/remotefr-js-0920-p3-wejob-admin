3e763c86ceed4ba71fa1ec952a93833fe600f57d
Emilie <martelemilie@hotmail.fr>
2021-01-29 09:04:14 +0100

    Add JobeurFormRecap component

---
+++ src/components/JobeurFormRecap.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function JobeurFormRecap({ handleSubmit, data }) {
+  console.log(data);
+  return (
+    <form onSubmit={handleSubmit} id="JobeurForm">
+      <div className="container">
+        <div className="divImgProfile">Photo</div>
+        <div className="infoJobeur">
+          <h2>
+            {data.firstname} {data.lastname}
+          </h2>
+        </div>
+      </div>
+    </form>
+  );
+}
+JobeurFormRecap.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  data: PropTypes.shape().isRequired,
+};
+
+export default JobeurFormRecap;
