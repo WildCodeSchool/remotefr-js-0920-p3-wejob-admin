9c90e148b193f9646510ce9f385383e1c5b917ef
Benoît Hubert <benoithubert@gmail.com>
2021-01-31 22:57:33 +0100

    Check auth state on app init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import axios from 'axios';
 import Header from './parts/Header/Header';
 import ChangePassword from './pages/Authentication/ChangePassword';
 import LogIn from './pages/Authentication/LogIn';
@@ -10,10 +11,48 @@ import JobeurForm from './pages/JobeurForm';
 import './asset/css/style.css';
 import './App.css';
 
-function App() {
+function AppLayout({ children }) {
   return (
     <div className="App">
       <Header />
+      {children}
+    </div>
+  );
+}
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [networkError, setNetworkError] = useState(null);
+
+  // quand l'app s'initialise, je vais faire une req. au back pour savoir si je suis authentifié
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/auth/check`)
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        if (!err.response) setNetworkError(err);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (networkError) {
+    return (
+      <AppLayout>
+        <div className="container">
+          <div className="alert alert-danger">
+            Une erreur s&#39;est produite : <code>{networkError.message}</code>.
+            Veuillez ré-essayer ultérieurement.
+          </div>
+        </div>
+      </AppLayout>
+    );
+  }
+  if (isLoading) {
+    return <AppLayout>Chargement...</AppLayout>;
+  }
+  return (
+    <AppLayout>
       <Switch>
         <Route path="/ForgotYourPassword">
           <ForgotYourPassword />
@@ -34,7 +73,8 @@ function App() {
           <JobeurForm />
         </Route>
       </Switch>
-    </div>
+      {!user && <Redirect to="/LogIn" />}
+    </AppLayout>
   );
 }
 
