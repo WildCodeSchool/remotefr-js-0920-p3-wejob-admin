555b6d55876a6d31174a34472c6d9b9ef73b3772
Benoît Hubert <benoithubert@gmail.com>
2021-02-07 16:30:44 +0100

    Sort recruiters by most recent visit, show details

--- src/pages/AdminPanel/ListRecrutors.jsx
+++ src/pages/AdminPanel/ListRecrutors.jsx
@@ -8,7 +8,16 @@ const fetchRecruteurs = () =>
     .get(`${process.env.REACT_APP_API_URL}/recruteurs`, {
       withCredentials: true,
     })
-    .then(({ data }) => data);
+    .then(({ data }) =>
+      data.sort((recA, recB) => {
+        if (!recA.visits && !recB.visits) return 0;
+        if (recA.visits && !recB.visits) return -1;
+        if (!recA.visits && recB.visits) return 1;
+        if (recA.visits[0] > recB.visits[0]) return -1;
+        if (recA.visits[0] < recB.visits[0]) return 1;
+        return 0;
+      }),
+    );
 
 const sanitize = (str) =>
   str
@@ -16,9 +25,18 @@ const sanitize = (str) =>
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '');
 
+const formatTimestamp = (ts) => {
+  const d = new Date(ts);
+  const m = d.toISOString().match(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})/);
+  if (!m) return 'Err';
+  const [, year, month, day, hour, min] = m;
+  return `${day}/${month}/${year} ${hour}:${min}`;
+};
+
 export default function ListRecrutors() {
   const [search, setSearch] = useState('');
   const [recruteurs, setRecruteurs] = useState([]);
+  const [selectedRecruteur, setSelectedRecruteur] = useState(null);
   const filteredRecruiters = useMemo(() => {
     const sanSearch = sanitize(search);
     return search === ''
@@ -38,25 +56,46 @@ export default function ListRecrutors() {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <div
-        className="overflow-auto d-flex flex-wrap mt-2"
-        style={{ maxHeight: '80vh' }}
-      >
-        {filteredRecruiters.map((r) => (
-          <Recrutor key={r.id} {...r} />
-        ))}
+      <div className="overflow-auto row mt-2" style={{ maxHeight: '80vh' }}>
+        <div className="col-md-8">
+          {filteredRecruiters.map((r) => (
+            <Recrutor
+              key={r.id}
+              {...r}
+              setSelected={() => setSelectedRecruteur(r)}
+            />
+          ))}
+        </div>
+        <div className="col-md-4">
+          <ul className="list-group">
+            {selectedRecruteur?.visits?.map((v) => (
+              <li className="list-group-item">{formatTimestamp(v)}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
 }
 
-const Recrutor = ({ email, name, telephone: number, visits }) => {
+const Recrutor = ({ email, name, telephone: number, visits, setSelected }) => {
+  const visitsCount = visits?.length || 0;
   return (
-    <div className="card m-3 p-3">
+    <div className="card mb-3 p-3">
       <h4 className="fw-bold">{name}</h4>
-      <h5>{email}</h5>
-      <h5>{number}</h5>
-      <p>visits: {visits?.length || 0}</p>
+      <p>
+        {email} &mdash; {number}
+      </p>
+      <p>
+        <button
+          type="button"
+          className="btn btn-sm btn-outline-primary"
+          onClick={setSelected}
+          disabled={visitsCount === 0}
+        >
+          Détail visites ({visitsCount})
+        </button>
+      </p>
     </div>
   );
 };
