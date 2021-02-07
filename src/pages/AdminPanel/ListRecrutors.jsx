302bb81e59625877b3efb899210a2de562e9a844
Benoît Hubert <benoithubert@gmail.com>
2021-02-07 16:10:46 +0100

    Get list of recruiters from backend

--- src/pages/AdminPanel/ListRecrutors.jsx
+++ src/pages/AdminPanel/ListRecrutors.jsx
@@ -1,10 +1,34 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useState } from 'react';
-import recrutors from './recrutors.json';
+import React, { useState, useEffect, useMemo } from 'react';
+import axios from 'axios';
+
+const fetchRecruteurs = () =>
+  axios
+    .get(`${process.env.REACT_APP_API_URL}/recruteurs`, {
+      withCredentials: true,
+    })
+    .then(({ data }) => data);
+
+const sanitize = (str) =>
+  str
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '');
 
 export default function ListRecrutors() {
   const [search, setSearch] = useState('');
+  const [recruteurs, setRecruteurs] = useState([]);
+  const filteredRecruiters = useMemo(() => {
+    const sanSearch = sanitize(search);
+    return search === ''
+      ? recruteurs
+      : recruteurs.filter((r) => sanitize(r.name).includes(sanSearch));
+  }, [recruteurs, search]);
+
+  useEffect(() => {
+    fetchRecruteurs().then(setRecruteurs);
+  }, []);
   return (
     <>
       <input
@@ -18,22 +42,21 @@ export default function ListRecrutors() {
         className="overflow-auto d-flex flex-wrap mt-2"
         style={{ maxHeight: '80vh' }}
       >
-        {search === ''
-          ? recrutors.map((r) => <Recrutor key={r.id} {...r} />)
-          : recrutors
-              .filter((r) => r.name.includes(search))
-              .map((r) => <Recrutor key={r.id} {...r} />)}
+        {filteredRecruiters.map((r) => (
+          <Recrutor key={r.id} {...r} />
+        ))}
       </div>
     </>
   );
 }
 
-const Recrutor = ({ email, name, number }) => {
+const Recrutor = ({ email, name, telephone: number, visits }) => {
   return (
     <div className="card m-3 p-3">
       <h4 className="fw-bold">{name}</h4>
       <h5>{email}</h5>
       <h5>{number}</h5>
+      <p>visits: {visits?.length || 0}</p>
     </div>
   );
 };
